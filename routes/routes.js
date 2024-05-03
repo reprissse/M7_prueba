e467ab6735264b6cfdfd4ead5f2e82cc7a95a380
Raquel Jimenez <raquel.jimenez.ramirez@gmail.com>
2024-05-02 23:50:07 -0400

    :sparkles: se actualizan comentarios

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,21 @@
+// Importamos el módulo express y las funciones del controlador de usuario
 import express from "express";
-import { home, addUser, getUser, editUser, deleteUser, addTranfer, getTranfer } from "../controller/userController.js";
+import 
+{ home, addUser, getUser, editUser, deleteUser, addTranfer, getTranfer } from "../controller/userController.js";
 
+// Creamos un nuevo router
 const router = express.Router();
 
-router.get("/", home)
-router.post("/usuario", addUser)
-router.get("/usuarios", getUser)
-router.put('/usuario', editUser)
-router.delete('/usuario', deleteUser)
-router.post('/transferencia', addTranfer)
-router.get('/transferencias', getTranfer)
+// Definimos las rutas y las funciones que se ejecutarán cuando se acceda a cada ruta
+router.get("/", home) // Ruta de inicio
+router.post("/usuario", addUser) // Ruta para agregar un usuario
+router.get("/usuarios", getUser) // Ruta para obtener todos los usuarios
+router.put('/usuario', editUser) // Ruta para editar un usuario
+router.delete('/usuario', deleteUser) // Ruta para eliminar un usuario
+router.post('/transferencia', addTranfer) // Ruta para agregar una transferencia
+router.get('/transferencias', getTranfer) // Ruta para obtener todas las transferencias
 
-export default router
+
+export default router;
+
 
